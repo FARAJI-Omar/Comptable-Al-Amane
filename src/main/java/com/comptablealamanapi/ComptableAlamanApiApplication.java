076ce4b38b50aea4d4421850e47a9909237809ad
F99 <farajiomar99@gmail.com>
2025-11-21 18:44:36 +0100

    Add main application class for Spring Boot setup

---
+++ src/main/java/com/comptablealamanapi/ComptableAlamanApiApplication.java
@@ -0,0 +1,13 @@
+package com.comptablealamanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComptableAlamanApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComptableAlamanApiApplication.class, args);
+    }
+
+}
